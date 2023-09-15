9b25a72530fd075297bf26d9d53fd4ee844936f1
Pstn4 <86513929+Pstn4@users.noreply.github.com>
2023-09-15 18:58:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+video="";
+status="";
 function preload()
 {
 
@@ -6,13 +8,23 @@ function setup()
 {
 canvas=createCanvas(480,380);
 canvas.center();
-
+video=createCapture(VIDEO);
+video.hide();
+video.size(480,380);
 }
 function draw()
 {
-
+image(video,0,0,480,380);
 }
 function start()
 {
-
+objectdetector=ml5.objectDetector("cocossd",modelloaded);
+document.getElementById("status").innerHTML="Status: Detecting Objects";
+objectname=document.getElementById("object").value;
+}
+function modelloaded()
+{
+    console.log("Model loaded!");
+    status=true;
+    console.log(objectname);
 }
